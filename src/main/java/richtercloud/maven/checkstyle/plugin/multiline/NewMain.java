70510dfbff7dc285ca4d881137981d73db565d39
Karl-Philipp Richter <krichter722@aol.de>
2016-04-25 23:38:15 +0200

    reproducing issue

---
+++ src/main/java/richtercloud/maven/checkstyle/plugin/multiline/NewMain.java
@@ -0,0 +1,20 @@
+/*
+ * a
+ * b
+ */
+package richtercloud.maven.checkstyle.plugin.multiline;
+
+/**
+ *
+ * @author richter
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/main/java/richtercloud/maven/checkstyle/plugin/multiline/NewMain1.java
@@ -0,0 +1,20 @@
+/*
+ * 1
+ * 2
+ */
+package richtercloud.maven.checkstyle.plugin.multiline;
+
+/**
+ *
+ * @author richter
+ */
+public class NewMain1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
